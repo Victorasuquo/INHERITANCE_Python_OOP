41141c5942ecc8e2bffe4813219ecec085e98032
Victorasuquo <jacobasuquo199@email.com>
2023-05-18 10:18:43 -0700

    my presentation

--- test_inheritance_script.py
+++ test_inheritance_script.py
@@ -1,195 +1,232 @@
-# #INHERITANCE IN PYTHON OOP
-# "----------------------------------------------------------------------"    
+#INHERITANCE IN PYTHON OOP
+"----------------------------------------------------------------------"    
 
-# #What is INHERITANCE IN PYTHON PROGRAMMING
-# """
-# In heritane is a common practise (in Object Orinted Programming) of passing attributes and
-#  methods from the Super class to the(defined and existing) to a ewly created class, called 
-#  the sub class, sibling or child class.
+#What is INHERITANCE IN PYTHON PROGRAMMING
+"""
+In heritane is a common practise (in Object Orinted Programming) of passing attributes and
+ methods from the Super class to the(defined and existing) to a ewly created class, called 
+ the sub class, sibling or child class.
 
-# It is a mechanism that allows you to create a hierarchy of classes that share a set of 
-# properties and methods by deriving a class from another class. Inheritance is the capability of
-# one class to derive or inherit the properties from another class. 
-# Any object bound to a specific level of hierachy inherits all the traits (as well as the attributes 
-# and qualities) defined inside any of the Superclass. Just like people, have parents, grand parents
-# and so on, objects have ancesstory. The principle of Inheritance lets a programmer to build
-# reationships bewteen concepts an group them together. In particuler this alows us to avoid
-# code duplicatioin by generalizing our code. 
+It is a mechanism that allows you to create a hierarchy of classes that share a set of 
+properties and methods by deriving a class from another class. Inheritance is the capability of
+one class to derive or inherit the properties from another class. 
+Any object bound to a specific level of hierachy inherits all the traits (as well as the attributes 
+and qualities) defined inside any of the Superclass. Just like people, have parents, grand parents
+and so on, objects have ancesstory. The principle of Inheritance lets a programmer to build
+reationships bewteen concepts an group them together. In particuler this alows us to avoid
+code duplicatioin by generalizing our code. 
  
-# Parent class is the class being inherited from, also called base class or super class.
+Parent class is the class being inherited from, also called base class or super class.
 
-# Child class is the class that inherits from base, parent or sper class, also called derived class.
-# """
+Child class is the class that inherits from base, parent or sper class, also called derived class.
+"""
 
-# #creating a base class and a sub class
-# class Vehicles:
-#     pass
+#creating a base class and a sub class
+class Vehicles:
+    pass
 		
-#     #base class
-# class LandVehicles(Vehicles):
-#       pass
+    #base class
+class LandVehicles(Vehicles):
+      pass
 
-# class WaterVehicles(Vehicles):
-#       pass
+class WaterVehicles(Vehicles):
+      pass
       
-# benz = LandVehicles()
+benz = LandVehicles()
 
-# """
+"""
 
-# The benefits of inheritance are:
+The benefits of inheritance are:
 
-# -It represents real-world relationships well and helps to build more specialised (more concrete)
-# classes using some sets of predefined general rules and behaviours.
+-It represents real-world relationships well and helps to build more specialised (more concrete)
+classes using some sets of predefined general rules and behaviours.
 
-# - it is a way of buiding a new class not from scratch but by using already predefined repetoire 
-#  of traits. 
-#  Land
-# -Also, it allows us to add more features to a class without modifying it.
-# -It is transitive in nature, which means that if class B inherits from another class A, then all
-#  the subclasses of B would automatically inherit from class A.
+- it is a way of buiding a new class not from scratch but by using already predefined repetoire 
+ of traits. 
+ Land
+-Also, it allows us to add more features to a class without modifying it.
+-It is transitive in nature, which means that if class B inherits from another class A, then all
+ the subclasses of B would automatically inherit from class A.
 
-# Types of Inheritance
-# Single Inheritance:
-# Single-level inheritance enables a derived class to inherit characteristics from a single-parent 
-# class.
+Types of Inheritance
+Single Inheritance:
+Single-level inheritance enables a derived class to inherit characteristics from a single-parent 
+class.
 
-# Multiple Inheritance:
-# Multiple level inheritance enables one derived class to inherit properties from more than one 
-# base class
+Multiple Inheritance:
+Multiple level inheritance enables one derived class to inherit properties from more than one 
+base class
 
-# Multilevel Inheritance:
-# Multi-level inheritance enables a derived class to inherit properties from an immediate parent 
-# class which in turn inherits properties from his parent class.
+Multilevel Inheritance:
+Multi-level inheritance enables a derived class to inherit properties from an immediate parent 
+class which in turn inherits properties from his parent class.
 
-# Hierarchical Inheritance:
-# Hierarchical level inheritance enables more than one derived class to inherit properties from a 
-# parent class.
+Hierarchical Inheritance:
+Hierarchical level inheritance enables more than one derived class to inherit properties from a 
+parent class.
 
-# Hybrid inheritance: This form combines more than one form of inheritance. 
-# Basically, it is a blend of more than one type of inheritance.
+Hybrid inheritance: This form combines more than one form of inheritance. 
+Basically, it is a blend of more than one type of inheritance.
 
-# """
-# """
-# #Attributes are characteristics associated with a type.
-# Methods are functons associated with a type.  To list all the attributes and methods of a class 
-# - use dir("") function
-# - help("") function to show all the documentation
-# """
+"""
+"""
+#Attributes are characteristics associated with a type.
+Methods are functons associated with a type.  To list all the attributes and methods of a class 
+- use dir("") function
+- help("") function to show all the documentation
+"""
 
-# #objects can be organised by inheritance
-# #EXAMPLE
-# """
-# We can generalise our class to be the Fruits class
-# """
+#objects can be organised by inheritance
+#EXAMPLE
+"""
+We can generalise our class to be the Fruits class
+"""
    
-# class Fruit:
-#     def __init__(self, color, flavor):
-#         self.color = color
-#         self.flavor = flavor
+class Fruit:
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
 
-# class Grape(Fruit):
-#     pass
+class Grape(Fruit):
+    pass
 
-# class Apple(Fruit):
-#     pass
-# #we told python to inherit all the methods and functions from the Base Class or 
-# # super Class "Fruit"
+class Apple(Fruit):
+    pass
+#we told python to inherit all the methods and functions from the Base Class or 
+# super Class "Fruit"
  
-# Granny_smith = Apple("green", "tart")
-# carnelian = Grape("purple", "sweet")
+Granny_smith = Apple("green", "tart")
+carnelian = Grape("purple", "sweet")
 
-# print(carnelian.color)
-# print(Granny_smith.flavor)
-
-
-# class Animal:
-#     sound = ""
-#     def __init__(self, name):
-#         self.name = name
-#     def speak(self):
-#         print("{sound}, I am {name}, {sound}".format(name = self.name, sound = self.sound))
+print(carnelian.color)
+print(Granny_smith.flavor)
 
 
+class Animal:
+    sound = ""
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        print("{sound}, I am {name}, {sound}".format(name = self.name, sound = self.sound))
 
-# class piglet(Animal):
-#     sound = "oink !"
-# hamlet = piglet("harmlet")
-# hamlet.speak()
 
-# class Cow(Animal):
-# 	sound = "Moooo"
+
+class piglet(Animal):
+    sound = "oink !"
+hamlet = piglet("harmlet")
+hamlet.speak()
+
+class Cow(Animal):
+	sound = "Moooo"
         
 
-# milky = Cow("Milky White")
-# milky.speak()
+milky = Cow("Milky White")
+milky.speak()
         
 
 
-# # Python code to demonstrate how parent constructors
-# # are called.
+# Python code to demonstrate how parent constructors
+# are called.
 
-# #Examples of inheritance 
-# # parent class
-# class Person(object):
+#Examples of inheritance 
+# parent class
+class Person(object):
 
-# 	# __init__ is known as the constructor
-# 	def __init__(self, name, idnumber):
-# 		self.name = name
-# 		self.idnumber = idnumber
+	# __init__ is known as the constructor
+	def __init__(self, name, idnumber):
+		self.name = name
+		self.idnumber = idnumber
 
-# 	def display(self):
-# 		print(self.name)
-# 		print(self.idnumber)
+	def display(self):
+		print(self.name)
+		print(self.idnumber)
 		
-# 	def details(self):
-# 		print("My name is {}".format(self.name))
-# 		print("IdNumber: {}".format(self.idnumber))
+	def details(self):
+		print("My name is {}".format(self.name))
+		print("IdNumber: {}".format(self.idnumber))
 	
-# # child class
-# class Employee(Person):
-# 	def __init__(self, name, idnumber, salary, post):
-# 		self.salary = salary
-# 		self.post = post
+# child class
+class Employee(Person):
+	def __init__(self, name, idnumber, salary, post):
+		self.salary = salary
+		self.post = post
 
-# 		# invoking the __init__ of the parent class
-# 		Person.__init__(self, name, idnumber)
+		# invoking the __init__ of the parent class
+		Person.__init__(self, name, idnumber)
 		
-# 	def details(self):
-# 		print("My name is {}".format(self.name))
-# 		print("IdNumber: {}".format(self.idnumber))
-# 		print("Post: {}".format(self.post))
+	def details(self):
+		print("My name is {}".format(self.name))
+		print("IdNumber: {}".format(self.idnumber))
+		print("Post: {}".format(self.post))
 
 
-# # creation of an object variable or an instance
-# a = Employee('Rahul', 886012, 200000, "Intern")
+# creation of an object variable or an instance
+a = Employee('Rahul', 886012, 200000, "Intern")
 
-# # calling a function of the class Person using
-# # its instance
-# print(a.display())
-# print(a.details())
-
-
-# # Output
-# # Rahul
-# # 886012
-# # My name is Rahul
-# # IdNumber: 886012
-# # Post: Intern
-# """In the above code, we have created two classes i.e. Person (parent class) and Employee 
-# (Child Class). The Employee class inherits from the Person class. We can use the methods of the
-#  person class through employee class as seen in the display function in the above code. 
-#  A child class can also modify the behavior of the parent class as seen through the details()
-# method."""
+# calling a function of the class Person using
+# its instance
+print(a.display())
+print(a.details())
 
 
+# Output
+# Rahul
+# 886012
+# My name is Rahul
+# IdNumber: 886012
+# Post: Intern
+"""In the above code, we have created two classes i.e. Person (parent class) and Employee 
+(Child Class). The Employee class inherits from the Person class. We can use the methods of the
+ person class through employee class as seen in the display function in the above code. 
+ A child class can also modify the behavior of the parent class as seen through the details()
+method."""
 
-# #EDUREKA
-# """the __init function is called automaticaly every time the the class is 
-# used to create an object"""
+# A Python program to demonstrate inheritance
 
-# #OVER RIDDING THE SUPERCLASS
-# """ we can intialize  a sub class by using the __init__() method and inherit all the
-# attributes from a super class by invoking the __init__()  """
+# Base or Super class. Note object in bracket.
+# (Generally, object is made ancestor of all classes)
+# In Python 3.x "class Person" is
+# equivalent to "class Person(object)"
+
+
+class Person(object):
+
+	# Constructor
+	def __init__(self, name):
+		self.name = name
+
+	# To get name
+	def getName(self):
+		return self.name
+
+	# To check if this person is an employee
+	def isEmployee(self):
+		return False
+
+
+# Inherited or Subclass (Note Person in bracket)
+class Employee(Person):
+
+	# Here we return true
+	def isEmployee(self):
+		return True
+
+
+# Driver code
+emp = Person("Geek1") # An Object of Person
+print(emp.getName(), emp.isEmployee())
+
+emp = Employee("Geek2") # An Object of Employee
+print(emp.getName(), emp.isEmployee())
+
+
+#EDUREKA
+"""the __init__ function is called automaticaly every time the the class is 
+used to create an object"""
+
+#OVER RIDDING THE SUPERCLASS
+""" we can intialize  a sub class by using the __init__() method and inherit all the
+attributes from a super class by invoking the __init__()  """
 
 
 class Super_name:
@@ -215,7 +252,7 @@ weve explicitly named the Superclass and pointed to the method to invoke __init_
 all the needed arguments. We have all instantiated one object of class sub and printed it
 
 """
-#we cann also use the super() functio to automatically inherit all the atrributes from the 
+#we can also use the super() functio to automatically inherit all the atrributes from the 
 #from the parent class which does not involve writting the Super class name
 
 class Super_name:
@@ -233,6 +270,40 @@ print(you)
 
 #more on class varibles: adding properties
 
+# parent class
+class Person():
+    def __init__(self, name, age):
+	    self.name = name
+	    # self.age = age
+
+def display(self):
+	print(self.name, self.age)
+
+# child class
+class Student(Person):
+	def __init__(self, name, age, dob):
+		self.sName = name
+		self.sAge = age
+		self.dob = dob
+		# inheriting the properties of parent class
+		super().__init__("Rahul", age)
+
+def displayInfo(self):
+	print(self.sName, self.sAge, self.dob)
+
+obj = Student("Mayank", 23, "16-03-2000")
+obj.display()
+obj.displayInfo()
+
+"""
+It's now possible to formulate a general statement describing Python's behavior.
+
+When you try to access any object's entity, Python will try to (in this order):
+
+find it inside the object itself;
+find it in all classes involved in the object's inheritance line from bottom to top;
+If both of the above fail, an exception (AttributeError) is raised
+"""
 
 class Dog:
     kennel = 0
